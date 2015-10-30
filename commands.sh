5684cb9c9f9e335bbb59e81786fdb26a1fab3483
Christian Gram Kalhauge <christian@kalhauge.dk>
2015-10-29 17:44:35 -0700

    Added mac os x stuff

--- commands.sh
+++ commands.sh
@@ -1,3 +1,31 @@
+
+function readlinkf () {
+    # Stolen from http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+    TARGET_FILE=$1
+
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+
+    # Iterate down a (possible) chain of symlinks
+    while [ -L "$TARGET_FILE" ]
+    do
+        TARGET_FILE=`readlink $TARGET_FILE`
+        cd `dirname $TARGET_FILE`
+        TARGET_FILE=`basename $TARGET_FILE`
+    done
+
+    # Compute the canonicalized name by finding the physical path 
+    # for the directory we're in and appending the target file.
+    PHYS_DIR=`pwd -P`
+    RESULT=$PHYS_DIR/$TARGET_FILE
+    echo $RESULT
+}
+
+
+function realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 function cmd () { 
     if [ "$STATE" = "DEBUG" ]; then
         echo "$@"
@@ -19,7 +47,7 @@ function debugf () {
 
 # register takes a file and a location
 function register () {
-    file=$(readlink -f $2)
+    file=$(readlinkf $2)
     config_file=$(realpath $1)
     if [ "$config_file" = "$file" ]; then
         printf "+ %s\n" $1
@@ -38,7 +66,7 @@ function register () {
 }
 
 function register_all () {
-    target=$(readlink -f $2)
+    target=$(readlinkf $2)
     source_folder=$(realpath $1)
 
     if [ -d $source_folder ]; then

285b82dfee5c6f022ec1d770f264825aec4728c8
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2019-12-15 16:22:12 +0100

    Add stuff

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3.6
 import re, subprocess
-def get_keychain_pass(account=None, server=None):
-    params = {
-        'security': '/usr/bin/security',
-        'command': 'find-internet-password',
-        'account': account,
-        'server': server,
-    }
-    command = "%(security)s -v %(command)s -g -a %(account)s -s %(server)s" % params
-    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
-    outtext = [l for l in output.splitlines()
-               if l.startswith('password: ')][0]
-
-    return re.match(r'password: "(.*)"', outtext).group(1)
+def get_keychain_pass(_file=None):
+    return subprocess.check_output("gpg --quiet --for-your-eyes-only --no-tty --decrypt " + _file, shell=True)
